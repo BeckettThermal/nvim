c8cef9afd9b41926a15aa153a6625483ece8df8c
Stadtmusiker <51013189+Stadtmusiker@users.noreply.github.com>
2026-05-29 14:42:26 -0400

    Adds autocommands (#1)
    Forks an established repo for Neovim and adds our autocommands for
    astyle and file headers.

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require("config.autoc")
 require("config.options")
 require("config.keybinds")
 require("manage").setup()
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -192,11 +192,11 @@ vim.lsp.config['rust_analyzer'] = {
 vim.lsp.config['clangd'] = {
     cmd = {
         'clangd',
-        -- '--background-index',
+        '--background-index',
         -- '--clang-tidy',
         -- '--header-insertion=never',
-        -- '--completion-style=detailed',
-        -- '--query-driver=/nix/store/*-gcc-*/bin/gcc*,/nix/store/*-clang-*/bin/clang*,/run/current-system/sw/bin/cc*',
+        '--completion-style=detailed',
+        '--query-driver=/nix/store/*-gcc-*/bin/*gcc*,/nix/store/*-clang-*/bin/clang*,/run/current-system/sw/bin/cc*,.',
     },
     filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
     root_markers = { 'compile_commands.json', '.clangd', 'configure.ac', 'Makefile', '.git' },
